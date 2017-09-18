031e4d124b4225a0efc2f95f7970c42e71fe5f4a
e200 <eleandro@inbox.ru>
2017-09-18 23:18:13 +0100

    Added DependencyFactoryTest

---
+++ tests/DependencyResolverFactoryTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Unity\Component\Container\Contracts\IDependencyResolver;
+use Unity\Component\Container\Contracts\IUnityContainer;
+use Unity\Component\Container\Dependency\DependencyResolverFactory;
+
+/**
+ * @author Eleandro Duzentos <eleandro@inbox.ru>
+ */
+class DependencyResolverFactoryTest extends TestCase
+{
+    function testMake()
+    {
+        $containerMock = $this->createMock(IUnityContainer::class);
+
+        $this->assertInstanceOf(
+            IDependencyResolver::class,
+            DependencyResolverFactory::Make('testId', 'e200', $containerMock)
+        );
+    }
+}
