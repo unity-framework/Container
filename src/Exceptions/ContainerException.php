24678a259cab82e4ae6f9af1e1e0c67074966baa
e200 <eleandro@inbox.ru>
2017-09-18 23:16:48 +0100

    Changed package namespace

--- src/Exceptions/ContainerException.php
+++ src/Exceptions/ContainerException.php
@@ -1,11 +1,18 @@
 <?php
 
-namespace Unity\Component\IoC\Exceptions;
+namespace Unity\Component\Container\Exceptions;
 
 use Exception;
-use Psr\Container\NotFoundExceptionInterface;
 use Throwable;
+use Psr\Container\NotFoundExceptionInterface;
 
+/**
+ * Class ContainerException.
+ *
+ * @package Unity\Component\Container\Exceptions
+ *
+ * @author Eleandro Duzentos <eleandro@inbox.ru>
+ */
 class ContainerException extends Exception implements NotFoundExceptionInterface
 {
     public function __construct($message = "", $code = 0, Throwable $previous = null)
