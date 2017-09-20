c6a2af4be63c4ecc43bbd47c3b21152c7183025c
e200 <eleandro@inbox.ru>
2017-09-20 15:17:21 +0100

    Added exception code 0200

--- src/Exceptions/MissingConstructorArgumentException.php
+++ src/Exceptions/MissingConstructorArgumentException.php
@@ -14,7 +14,7 @@ use Unity\Component\Container\Contracts\IMissingConstructorArgumentException;
  */
 class MissingConstructorArgumentException extends Exception implements IMissingConstructorArgumentException
 {
-    public function __construct($message = '', $code = 0, Throwable $previous = null)
+    public function __construct($message = '', $code = 0200, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
