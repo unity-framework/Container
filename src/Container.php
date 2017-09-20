498c29beca43897ed6b29d7ea2edeebd0db799c4
e200 <eleandro@inbox.ru>
2017-09-20 15:16:15 +0100

    Updated PHPdocs

--- src/Container.php
+++ src/Container.php
@@ -13,70 +13,21 @@ use Unity\Component\Container\Exceptions\NotFoundException;
 /**
  * Class Container.
  *
+ * Dependencies manager.
  *
  * @author Eleandro Duzentos <eleandro@inbox.ru>
  */
 class Container implements IContainer
 {
-    protected $resolvers = [];
-    protected $autowiring = true;
-
-    /**
-     * Resolves and returns the dependency registered on the first call.
-     * Returns only the resolved dependency on subsequent calls.
-     *
-     * @param string $id Identifier of the dependency resolver.
-     *
-     * @throws NotFoundExceptionInterface
-     * @throws ContainerExceptionInterface
-     *
-     * @return mixed
-     */
-    public function get($id)
-    {
-        if ($this->has($id)) {
-            return $this->getDependencyResolver($id)->getSingleton();
-        }
-
-        throw new NotFoundException("No dependency resolver founded for \"{$id}\" on the container.");
-    }
-
-    /**
-     * Checks if the container has a dependency resolver for the given $id.
-     *
-     * @param string $id Identifier for the resolver.
-     *
-     * @return bool
-     */
-    public function has($id)
-    {
-        return isset($this->resolvers[$id]);
-    }
-
-    /**
-     * Resolves and returns the registered dependency on every call.
-     *
-     * @param $id
-     * @param null $params
-     *
-     * @throws NotFoundException
-     *
-     * @return mixed
-     */
-    public function make($id, $params = null)
-    {
-        if ($this->has($id)) {
-            return $this->getDependencyResolver($id)->make($params);
-        }
-
-        throw new NotFoundException("No dependency resolver founded for \"{$id}\" on this container.");
-    }
+    protected $needs      = [];
+    protected $resolvers  = [];
+    protected $autoInject = true;
 
     /**
      * Register a dependency resolver.
      *
      * @param string $id
-     * @param mixed  $entry The content that will be used to generate the dependency.
+     * @param mixed  $entry Content that will be used to generate the dependency.
      *
      * @throws DuplicateIdException
      *
@@ -87,6 +38,7 @@ class Container implements IContainer
         if ($this->has($id)) {
             throw new DuplicateIdException("The container already has a dependency resolver for \"{$id}\".");
         }
+
         return $this->setDependencyResolver($id, DependencyResolverFactory::make($id, $entry, $this));
     }
 
@@ -102,20 +54,72 @@ class Container implements IContainer
     public function unregister($id)
     {
         if (!$this->has($id)) {
-            throw new NotFoundException("No dependency resolver founded for \"{$id}\" on this container.");
+            $this->throwNotFoundException($id);
         }
+
         unset($this->resolvers[$id]);
 
         return $this;
     }
 
     /**
+     * Resolves and returns the dependency on the first call.
+     * Returns the resolved dependency on subsequent calls.
+     *
+     * @param string $id Dependency resolver identifier.
+     *
+     * @throws NotFoundExceptionInterface
+     * @throws ContainerExceptionInterface
+     *
+     * @return mixed
+     */
+    public function get($id)
+    {
+        if ($this->has($id)) {
+            return $this->getDependencyResolver($id)->getSingleton();
+        }
+
+        $this->throwNotFoundException($id);
+    }
+
+    /**
+     * Checks if the container has a dependency resolver for the given $id.
+     *
+     * @param string $id Dependency resolver identifier.
+     *
+     * @return bool
+     */
+    public function has($id)
+    {
+        return isset($this->resolvers[$id]);
+    }
+
+    /**
+     * Resolves and returns the registered dependency on every call.
+     *
+     * @param string     $id     Dependency resolver identifier.
+     * @param array|null $params
+     *
+     * @throws NotFoundException
+     *
+     * @return mixed
+     */
+    public function make($id, $params = null)
+    {
+        if ($this->has($id)) {
+            return $this->getDependencyResolver($id)->make($params);
+        }
+
+        $this->throwNotFoundException($id);
+    }
+
+    /**
      * Replaces a registered resolver.
      *
-     * This method does'nt replaces dependencies already resolved by this container.
+     * This method does'nt replaces dependencies already resolved by the container.
      *
      * @param string $id
-     * @param mixed  $entry The content that will be used to resolve the dependency.
+     * @param mixed  $entry Content that will be used to resolve the dependency.
      *
      * @return IDependencyResolver
      */
@@ -150,22 +154,39 @@ class Container implements IContainer
     }
 
     /**
-     * Enable|Disable autowiring.
+     * Enable|Disable auto inject.
+     *
+     * Tells the container if it should try auto inject
+     * classes constructor dependencies.
      *
      * @param bool $enable
+     *
+     * @return $this
      */
-    public function enableAutowiring($enable)
+    public function enableAutoInject($enable)
     {
-        $this->autowiring = $enable;
+        $this->autoInject = $enable;
+
+        return $this;
     }
 
     /**
-     * Checks if autowiring is enabled.
+     * Checks if auto inject is enabled.
      *
      * @return bool
      */
-    public function canAutowiring()
+    public function canAutoInject()
     {
-        return $this->autowiring;
+        return $this->autoInject;
+    }
+
+    /**
+     * @param $id
+     *
+     * @throws NotFoundException
+     */
+    public function throwNotFoundException($id)
+    {
+        throw new NotFoundException("No dependency resolver was founded for \"{$id}\" on the container.");
     }
 }
