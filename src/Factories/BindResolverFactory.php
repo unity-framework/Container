1b66981717fd506c818ad545a8dc06b42e676191
e200 <eleandro@inbox.ru>
2017-10-06 22:48:30 +0100

    Added BindResolverFactory

---
+++ src/Factories/BindResolverFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Unity\Component\Container\Factories;
+
+use Closure;
+use Unity\Component\Container\Contracts\IBindResolverFactory;
+use Unity\Component\Container\Contracts\IContainer;
+use Unity\Component\Container\Bind\BindResolver;
+
+/**
+ * Class BindResolverFactoryFactory.
+ *
+ * Makes BindResolverFactory instances.
+ *
+ * @author Eleandro Duzentos <eleandro@inbox.ru>
+ */
+class BindResolverFactory implements IBindResolverFactory
+{
+    public function make(Closure $callback, IContainer $container)
+    {
+        return new BindResolver($callback, $container);
+    }
+}
