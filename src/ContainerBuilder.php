9604157aa324faea64a8d16103ff3d7c7361c4e7
e200 <eleandro@inbox.ru>
2017-10-06 22:16:17 +0100

    Changed the way dependency are injected

--- src/ContainerBuilder.php
+++ src/ContainerBuilder.php
@@ -1,23 +1,32 @@
 <?php
 
 use Unity\Component\Container\Container;
+use Unity\Component\Container\Dependency\DependencyFactory;
+use Unity\Component\Container\Factories\BindResolverFactory;
+use Unity\Component\Container\Factories\DependencyResolverFactory;
 use Unity\Reflector\Reflector;
 
+/**
+ * Class ContainerBuilder.
+ *
+ * Container builder.
+ *
+ * @author Eleandro Duzentos <eleandro@inbox.ru>
+ */
 class ContainerBuilder
 {
     public static function build()
     {
-        $container = new Container();
+        $dependencyFactory = new DependencyFactory(new Reflector());
+        $dependencyResolverFactory = new DependencyResolverFactory();
+        $bindResolverFactory = new BindResolverFactory();
 
-        $dependencyFactory = $self::getDependencyFactory($container);
-
-        $container->setDependencyFactory($dependencyFactory);
+        $container = new Container(
+            $dependencyFactory,
+            $dependencyResolverFactory,
+            $bindResolverFactory
+        );
 
         return $container;
-    }
-
-    public static function getDependencyFactory(Container $container)
-    {
-        return new DependencyFactory($container, new Reflector());
     }
 }
