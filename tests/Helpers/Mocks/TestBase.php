e804bb011b4d4d17c0240af1d9d8236eea2d2b45
e200 <eleandro@inbox.ru>
2017-10-06 22:47:34 +0100

    Added mock for DependencyFactory

--- tests/Helpers/Mocks/TestBase.php
+++ tests/Helpers/Mocks/TestBase.php
@@ -4,7 +4,10 @@ namespace Helpers\Mocks;
 
 use PHPUnit\Framework\TestCase;
 use Unity\Component\Container\Container;
-use Unity\Component\Container\Dependency\DependencyFactory;
+use Unity\Component\Container\Contracts\IBindResolverFactory;
+use Unity\Component\Container\Contracts\IDependencyFactory;
+use Unity\Component\Container\Contracts\IDependencyResolver;
+use Unity\Component\Container\Contracts\IDependencyResolverFactory;
 
 class TestBase extends TestCase
 {
@@ -15,6 +18,54 @@ class TestBase extends TestCase
 
     public function mockDependencyFactory()
     {
-        return $this->createMock(DependencyFactory::class);
+        return $this->createMock(IDependencyFactory::class);
+    }
+
+    public function mockDependencyResolverFactory()
+    {
+        $df = $this->createMock(IDependencyResolver::class);
+
+        $df
+            ->expects($this->any())
+            ->method('resolve')
+            ->willReturn(true);
+
+        $df
+            ->expects($this->any())
+            ->method('make')
+            ->willReturn(true);
+
+        $drf = $this->createMock(IDependencyResolverFactory::class);
+
+        $drf
+            ->expects($this->any())
+            ->method('make')
+            ->willReturn($df);
+
+        return $drf;
+    }
+
+    public function mockBindResolverFactory()
+    {
+        $br = $this->createMock(IDependencyResolver::class);
+
+        $br
+            ->expects($this->any())
+            ->method('resolve')
+            ->willReturn(true);
+
+        $br
+            ->expects($this->any())
+            ->method('make')
+            ->willReturn(true);
+
+        $brf = $this->createMock(IBindResolverFactory::class);
+
+        $brf
+            ->expects($this->any())
+            ->method('make')
+            ->willReturn($br);
+
+        return $brf;
     }
 }
