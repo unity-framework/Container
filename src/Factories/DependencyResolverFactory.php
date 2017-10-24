c68a62939d5de9e0021eeab3acc5c36e6001b218
e200 <eleandro@inbox.ru>
2017-10-24 13:38:59 +0100

    Added `IBindResolver::class` as `make()` argument.

--- src/Factories/DependencyResolverFactory.php
+++ src/Factories/DependencyResolverFactory.php
@@ -4,6 +4,7 @@ namespace Unity\Component\Container\Factories;
 
 use Unity\Component\Container\Dependency\DependencyResolver;
 use Unity\Contracts\Container\Dependency\IDependencyFactory;
+use Unity\Contracts\Container\Factories\IBindResolverFactory;
 use Unity\Contracts\Container\Factories\IDependencyResolverFactory;
 use Unity\Contracts\Container\IContainer;
 
@@ -19,11 +20,13 @@ class DependencyResolverFactory implements IDependencyResolverFactory
     public function make(
         $entry,
         IDependencyFactory $dependencyFactory,
+        IBindResolverFactory $bindResolverFactory,
         IContainer         $container
     ) {
         return new DependencyResolver(
             $entry,
             $dependencyFactory,
+            $bindResolverFactory,
             $container
         );
     }
